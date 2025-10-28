771b51d508242aac5da91c7c49a96eaff9bb1a61
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-28 10:48:53 -0300

    added namespace in livox mid360 launch

--- launch/msg_MID360_launch.py
+++ launch/msg_MID360_launch.py
@@ -33,10 +33,13 @@ livox_ros2_params = [
 
 
 def generate_launch_description():
+    uav_name = os.environ['UAV_NAME']
+
     livox_driver = Node(
         package='livox_ros_driver2',
         executable='livox_ros_driver2_node',
         name='livox_lidar_publisher',
+        namespace=uav_name
         output='screen',
         parameters=livox_ros2_params
         )
